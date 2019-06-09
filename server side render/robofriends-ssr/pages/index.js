61e7efd22312fb65149521fedec7646070fa2e35
waraphon roonnapai <roonnapai.dev@gmail.com>
2019-06-09 13:45:35 +0700

    add robofriend with nextJS code.

--- server side render/robofriends-ssr/pages/index.js
+++ server side render/robofriends-ssr/pages/index.js
@@ -1,9 +1,57 @@
-const Index = () => {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+import React, { Component } from 'react';
+import Head from 'next/head';
+import axios from 'axios';
+
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+export class index extends Component {
+  state = {
+    searchField: ""
+  }
+
+  static async getInitialProps() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    const { data } = response;
+    return { robots: data };
+  }
+
+  onSearchChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    const { robots } = this.props;
+    const { searchField } = this.state;
+    const isPending = false;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+
+    return (
+      <div>
+        <Head>
+          <title>RoboFriends -SSR</title>
+          <link href="/static/index.css" rel="stylesheet" />
+          <link href="/static/App.css" rel="stylesheet" />
+          <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
+        </Head>
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={e => this.onSearchChange(e)}/>
+          <Scroll>
+            {
+              <ErrorBoundry>
+                <CardList robots={filteredRobots} />
+              </ErrorBoundry>
+            }
+          </Scroll>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Index
+export default index;
